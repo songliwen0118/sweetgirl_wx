2f0e2358b7903cbc8852ad979c03e25174457a73
songlw <songlw>
2019-06-12 16:30:47 +0800

    测试
    测试插件样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,25 @@
 //index.js
 //获取应用实例
 const app = getApp()
+//初始化一个文字参数
+var initText = '这是第一个文字\n这是第二个文字'
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    text: initText,
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+    iconType: [
+      'success', 'info', 'warn', 'waiting', 'safe_success', 'safe_warn',
+      'success_circle', 'success_no_circle', 'waiting_circle', 'circle', 'download',
+      'info_circle', 'cancel', 'search', 'clear'
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +27,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,6 +55,11 @@ Page({
       })
     }
   },
+  onGotUserInfo: function(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+  },
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -50,5 +67,28 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  //初始化一个空的文字串
+  extraLine: [],
+  //添加按钮点击事件
+  add: function(e) {
+    //在文字串中添加文字，push
+    this.extraLine.push('添加的其他文字')
+    //设置数据
+    this.setData({
+      text: initText + '\n' + this.extraLine.join('\n')
+    })
+  },
+  //减少按钮点击事件
+  remove: function(e) {
+    //判断文字串是否大于0，如果大于0，减少，反之，不操作
+    if (this.extraLine.length > 0) {
+      //在文字串中减少文字，pop
+      this.extraLine.pop()
+      //设置数据
+      this.setData({
+        text: initText + '\n' + this.extraLine.join('\n')
+      })
+    }
   }
 })
